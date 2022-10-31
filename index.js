dfb1c6e7fbf83f6f5fb254d7703230d592930467
Tanwish <cse210001072@iiti.ac.in>
2022-11-01 01:54:49 +0530

    navbar commit

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path=require('path')
 
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'/views'))
+app.set('public',path.join(__dirname,'/public'))
 
 const db=mysql.createConnection({
     host:'127.0.0.1',
@@ -30,6 +31,47 @@ app.get('/',(req,res)=>{
    res.render("login.ejs")
 });
 
+
+app.get('/views/dashboard',(req,res)=>{
+    
+
+    res.render("dashboard.ejs")
+ });
+ app.get('/views/orders',(req,res)=>{
+    
+
+    res.render("orders.ejs")
+ });
+ app.get('/views/KOT',(req,res)=>{
+    
+
+    res.render("KOT.ejs")
+ });
+ app.get('/views/Suppliers',(req,res)=>{
+    
+
+    res.render("Suppliers.ejs")
+ });
+ app.get('/views/Reservation',(req,res)=>{
+    
+
+    res.render("Reservation.ejs")
+ });
+ app.get('/views/Inventory',(req,res)=>{
+    
+
+    res.render("Inventory.ejs")
+ });
+ app.get('/views/Customer_satisfaction',(req,res)=>{
+    
+
+    res.render("Customer_satisfaction.ejs")
+ });
+ app.get('/views/Menu',(req,res)=>{
+    
+
+    res.render("Menu.ejs")
+ });
 // app.get('/login', (req, res)=>
 // {
 //     if(  (req.query.username==null)|| (req.query.password==null)){
@@ -62,6 +104,8 @@ app.get('/login', (req, res)=>{
     var username=req.query.username
     var password=req.query.password
     let sql=`select * from staff2 where username='${username}'`
+    // .js
+
 
     db.query(sql, (err, result)=>{
         if(err) console.log(err)
@@ -79,6 +123,7 @@ app.get('/login', (req, res)=>{
         }
         else
         {
+            res.locals(prom)
             res.render('login.ejs')
             // prompt("invali password")
         }
